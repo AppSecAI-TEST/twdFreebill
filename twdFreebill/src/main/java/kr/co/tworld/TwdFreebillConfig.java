e3a62abcd2b15f9b23042acffe4e86b8e49e301d
Administrator <Administrator@SKCC13N00760.SKCC.NET>
2017-05-02 09:27:00 +0900

    데이터베이스 서비스명으로 연결

--- twdFreebill/src/main/java/kr/co/tworld/TwdFreebillConfig.java
+++ twdFreebill/src/main/java/kr/co/tworld/TwdFreebillConfig.java
@@ -1,5 +1,7 @@
 package kr.co.tworld;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
@@ -14,6 +16,9 @@ public class TwdFreebillConfig extends AbstractCloudConfig {
 
 	@Value("${services.redis.name}")
 	private String redisName;
+
+	@Value("${services.database.name}")
+	private String databaseName;
 	
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
@@ -25,7 +30,12 @@ public class TwdFreebillConfig extends AbstractCloudConfig {
 	    RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
 	    template.setConnectionFactory(redisConnectionFactory());
 	    return template;
-	}	
+	}
+	
+	@Bean
+	public DataSource dataSource() {
+	    return connectionFactory().dataSource(databaseName);
+	}
 	
 	@Bean
 	@LoadBalanced
